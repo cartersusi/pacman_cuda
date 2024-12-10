4e5ecaad805f330bfee070e1b01ba35167f22f23
Carter Susi <carter4299@gmail.com>
2024-12-10 12:07:28 -0600

    update script

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -2,6 +2,8 @@ use std::process::{Command, Stdio};
 use std::panic;
 
 use serde::{Deserialize, Serialize};
+use dialoguer::{Select, theme::ColorfulTheme};
+use dialoguer::console::{Term, Style, Color};
 use ctrlc;
 
 const JSONDATA: &[u8] = include_bytes!("data/packages.json");
@@ -34,42 +36,64 @@ enum Lvl {
     Warning,
     Success,
     Error,
+    Standard,
 }
 
-fn log(msg_type: Lvl, msg: &str) -> String {
+fn log(msg_type: Lvl, msg: &str) {
     let color_code = match msg_type {
-        Lvl::Warning => "0;33",
-        Lvl::Success => "0;32",
-        Lvl::Error => "0;31",
+        Lvl::Warning => Style::new().fg(Color::Yellow).bold(),
+        Lvl::Success => Style::new().fg(Color::Green).bold(),
+        Lvl::Error => Style::new().fg(Color::Red).bold(),
+        Lvl::Standard => Style::new().fg(Color::White).bold(),
     };
-    format!("\x1b[{}m{}\x1b[0m", color_code, msg)
+    Term::stdout().write_line(&color_code.apply_to(msg).to_string()).unwrap();
 }
 
 fn main() {
     ctrlc::set_handler(|| {
-        println!("{}", log(Lvl::Error, "Installation was interrupted by the user"));
+        log(Lvl::Error, "Installation was interrupted by the user. Cleaning up...");
         end(true);
     }).expect("Error setting Ctrl+C handler");
 
     let result = panic::catch_unwind(|| {
-        let json_from_bytes = std::str::from_utf8(JSONDATA).expect(&log(Lvl::Error, "Failed to convert JSON data to string"));
-        let data: Data = serde_json::from_str(json_from_bytes).expect(&log(Lvl::Error, "Failed to parse JSON file"));
+        let json_from_bytes = std::str::from_utf8(JSONDATA).expect("Failed to parse JSON file");
+        let data: Data = serde_json::from_str(json_from_bytes).expect("Failed to parse JSON file");
 
         data.start();
 
-        println!("Would you like to install the recent or compatible version?\n1. Recent\n2. Compatible");
+        let versions = &["Recent", "Compatible"];
+        let versions = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Would you like to install the recent or compatible version?")
+            .items(&versions[..])
+            .default(0)
+            .interact()
+            .expect("Failed to get user choice");
 
-        let c = get_choice(3, "1", "2").expect(&log(Lvl::Error, "Failed to get choice"));
-        let c = c.parse::<i32>().expect(&log(Lvl::Error, "Failed to parse choice"));
-
-        println!("Supporting packages for `{}` version:", CHOICES[c as usize - 1]);
+        let c = versions + 1;
+        log(Lvl::Standard, &format!("\nSupporting packages for `{}` version:", CHOICES[c as usize - 1]));
         let deps = match c {
             1 => &data.recent,
             2 => &data.compatible,
-            _ => panic!("{}", log(Lvl::Error, "Invalid choice")),
+            _ => {
+                log(Lvl::Error, "Invalid Choice");
+                panic!();
+            }
         };
 
         deps.print_support();
+
+        let cont = &["Yes", "No"];
+        let cont = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Would you like to proceed with the installation?")
+            .items(&cont[..])
+            .default(0)
+            .interact()
+            .expect("Failed to get user choice");
+
+        if cont == 1 {
+            end(true);
+        }
+
         deps.download();
         deps.install();
 
@@ -87,16 +111,19 @@ impl Data {
     
         let cmd = format!("mkdir -p {}", TMPDIR);
         match run(&cmd, false) {
-            Ok(output) => println!("Command output: {}", output),
-            Err(error) => panic!("{}", log(Lvl::Error, &format!("Failed to create tmp directory: {}", error))),
+            Ok(_) => log(Lvl::Success, &format!("Tmp directory created: {}", TMPDIR)),
+            Err(error) => {
+                log(Lvl::Error, &format!("Failed to create tmp directory: {}", error));
+                panic!();
+            }
         }
     }
 
     fn install_deps(&self) {
         let cmd = format!("sudo pacman -Syu {}", &self.deps);
         match run(&cmd, true) {
-            Ok(output) => println!("Command output: {}", output),
-            Err(error) => eprintln!("{}", log(Lvl::Error, &format!("Failed to install dependencies: {}", error))),
+            Ok(_) => log(Lvl::Success, &format!("Dependencies installed successfully: {}", &self.deps)),
+            Err(error) => log(Lvl::Error, &format!("Failed to install dependencies: {}", error)),
         }
     }
 }
@@ -113,30 +140,41 @@ impl Deps {
 
     fn print_support(&self) {
         for supported in self.support.iter() {
-            println!("{}", supported);
+            log(Lvl::Standard, &format!("\t{}", supported));
         }
+        println!();
     }
 
     fn download(&self) {
         for (link, name) in self.packages() {
             if !verify_link(link) {
-                panic!("{}", log(Lvl::Error, "Invalid link"));
+                log(Lvl::Error, "Invalid link");
+                panic!();
             }
             let cmd = format!("curl -o {}/{} {}", TMPDIR, name, link);
             match run(&cmd, true) {
-                Ok(output) => println!("{}", log(Lvl::Success, &format!("Package downloaded successfully: {}", output))),
-                Err(error) => panic!("{}", log(Lvl::Error, &format!("Failed to download package: {}", error))),
+                Ok(_) => log(Lvl::Success, &format!("Package downloaded successfully: {}", name)),
+                Err(error) => {
+                    log(Lvl::Error, &format!("Failed to download package: {}", error));
+                    panic!();
+                }
             }
         }
     }
 
     fn install(&self) {
-        for (_, name) in self.packages() {
-            let cmd = format!("sudo pacman -U {}/{}", TMPDIR, name);
-            match run(&cmd, true) {
-                Ok(output) => println!("{}", log(Lvl::Success, &format!("Package installed successfully: {}", output))),
-                Err(error) => panic!("{}", log(Lvl::Error, &format!("Failed to install package: {}", error))),
-            }
+        let gcc = format!("{}/{} {}/{}", TMPDIR, self.gcc.name, TMPDIR, self.gcc_libs.name);
+        let gcccmd = format!("sudo pacman -U {}", gcc);
+        match run(&gcccmd, true) {
+            Ok(_) => log(Lvl::Success, &format!("Package installed successfully: {}, {}", self.gcc.name, self.gcc_libs.name)),
+            Err(error) => log(Lvl::Error, &format!(" Packages were not installed successfully: {}", error)),
+        }
+
+        let cuda = format!("{}/{} {}/{}", TMPDIR, self.cuda.name, TMPDIR, self.cudnn.name);
+        let cudacmd = format!("sudo pacman -U {}", cuda);
+        match run(&cudacmd, true) {
+            Ok(_) => log(Lvl::Success, &format!("Package installed successfully: {}, {}", self.cuda.name, self.cudnn.name)),
+            Err(error) => log(Lvl::Error, &format!(" Packages were not installed successfully: {}", error)),
         }
     }
 }
@@ -161,7 +199,7 @@ fn run(cmd: &str, interactive: bool) -> Result<String, String> {
     match output {
         Ok(output) => {
             if output.status.success() {
-                println!("{}", log(Lvl::Success, "Command executed successfully"));
+                log(Lvl::Success, "Command executed successfully");
                 Ok(String::from_utf8_lossy(&output.stdout).to_string())
             } else {
                 let exit_code = output.status.code().unwrap_or(-1);
@@ -185,33 +223,11 @@ fn run(cmd: &str, interactive: bool) -> Result<String, String> {
     }
 }
 
-fn get_choice(tries: i32, c1: &str, c2: &str) -> Result<String, String> {
-    let mut choice = String::new();
-    for _ in 0..tries {
-        match std::io::stdin().read_line(&mut choice) {
-            Ok(_) => {
-                choice = choice.trim().to_string();
-                if choice == c1 || choice == c2 {
-                    return Ok(choice);
-                }
-                choice.clear();
-                eprintln!("{}", log(Lvl::Warning, "Invalid choice"));
-            }
-            Err(error) => {
-                choice.clear();
-                eprintln!("Failed to read input: {}", error);
-            }
-        }
-    }
-
-    Err("Failed to read input".to_string())
-}
-
 fn verify_link(link: &str) -> bool {
     let cmd = format!("curl -I {}", link);
     match run(&cmd, false) {
         Ok(output) => if output.contains("HTTP/2 200") {
-            println!("{}" , log(Lvl::Success, "Link is valid"));
+            log(Lvl::Success, "Valid link");
             return true;
         },
         Err(error) => eprintln!("Failed to check link: {}", error),
@@ -223,17 +239,24 @@ fn verify_link(link: &str) -> bool {
 fn end(panicking: bool) {
     let cmd = format!("rm -rf {}", TMPDIR);
     match run(&cmd, false) {
-        Ok(output) => println!("Command output: {}", output),
-        Err(error) => panic!("Failed to remove tmp directory: {}", error),
+        Ok(_) => log(Lvl::Success, &format!("Tmp directory removed: {}", TMPDIR)),
+        Err(error) => {
+            log(Lvl::Error, &format!("Failed to remove tmp directory: {}", error));
+            log(Lvl::Warning, &format!("Please remove the tmp directory manually at {}", TMPDIR));
+
+        }
     }
 
     if panicking {
-        println!("{}", log(Lvl::Error, "Installation failed!"));
+        log(Lvl::Error, "Installation exited!");
         std::process::exit(1);
     }
-    println!("{}", log(Lvl::Success, "Installation completed successfully!"));
-    println!("To prevent the system from updating the packages, add the following to /etc/pacman.conf");
-    println!("\tIgnorePkg = cuda cudnn gcc12 gcc12-libs");
-    println!("Common Error: # ERROR: libdevice not found at ./libdevice.10.bc");
-    println!("To fix this error, run the following command:\n\texport XLA_FLAGS=--xla_gpu_cuda_data_dir=/opt/cuda");
+    log(Lvl::Success, "Installation completed successfully!");
+
+    println!("\nTo prevent the system from updating the packages, add the following to /etc/pacman.conf");
+    log(Lvl::Standard, "\tIgnorePkg = cuda cudnn gcc12 gcc12-libs");
+
+    println!("\nCommon Error\n# ERROR: libdevice not found at ./libdevice.10.bc");
+    println!("To fix this error, run the following command:");
+    log(Lvl::Standard, "\texport XLA_FLAGS=--xla_gpu_cuda_data_dir=/opt/cuda");
 }
